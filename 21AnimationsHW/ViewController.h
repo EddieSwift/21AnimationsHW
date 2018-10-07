583e6707f0fbcc6baa5bda44fe7429084496ca43
EddieSwift <37484195+EddieSwift@users.noreply.github.com>
2018-10-07 22:33:09 +0300

    Initial Commit

---
+++ 21AnimationsHW/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  21AnimationsHW
+//
+//  Created by Eduard Galchenko on 10/7/18.
+//  Copyright © 2018 Eduard Galchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
